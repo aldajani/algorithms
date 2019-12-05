3ffbad68dd5ef36712f509c40158d8d246cce1ae
aldajani <adajani001@gmail.com>
2019-12-05 01:49:00 -0600

    Implement MarsExploration

---
+++ MarsExploration.js
@@ -0,0 +1,19 @@
+const marsExploration = (s) => {
+  let charCount = 0;
+
+  for (let i = 0; i < s.length; i += 3) {
+      if (s[i] !== 'S') {
+          charCount++;
+      }
+      if (s[i + 1] !== 'O') {
+          charCount++;
+      }
+       if (s[i + 2] !== 'S') {
+          charCount++;
+      }
+  }
+  
+  return charCount;
+}
+
+console.log(marsExploration('SOOSSO')) // Answer = 3
