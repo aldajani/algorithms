c62bf617dc9b8c51c07136adf3d384ce46d82484
aldajani <adajani001@gmail.com>
2019-12-05 01:29:25 -0800

    Implement HackerrankString

---
+++ HackerrankString.js
@@ -0,0 +1,14 @@
+const hackerrankInString = (s) => {
+  let match = 'hackerrank'.split('');
+  for (const char of s) {
+      if (char === match[0]) {
+          match.shift()
+      }
+  }
+  if (match.length === 0) {
+      return 'YES';
+  }
+  return 'NO';
+}
+
+console.log(hackerrankInString('hereiamstackerrank')) // Answer = 'YES'
