48d773447bf2d98893953677c4544afd4a1c9951
aldajani <adajani001@gmail.com>
2019-12-05 01:29:59 -0800

    Fix spacing

--- HackerrankString.js
+++ HackerrankString.js
@@ -1,13 +1,15 @@
 const hackerrankInString = (s) => {
   let match = 'hackerrank'.split('');
+  
   for (const char of s) {
-      if (char === match[0]) {
-          match.shift()
-      }
+    if (char === match[0]) {
+      match.shift()
+    }
   }
   if (match.length === 0) {
-      return 'YES';
+    return 'YES';
   }
+
   return 'NO';
 }
 
