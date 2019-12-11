3b8eaccf8c3923d8582725ac9287c708a3416197
aldajani <adajani001@gmail.com>
2019-12-11 11:43:33 -0800

    Add test and fix formatting

--- RoundedGrades.js
+++ RoundedGrades.js
@@ -1,17 +1,19 @@
 const gradingStudents = (grades) => {
   let roundedGrades = [];
   for (let i = 0; i < grades.length; i++) {
-      if (grades[i] < 38) {
-          roundedGrades.push(grades[i])
-      } else if (grades[i] % 5 === 0) {
-          roundedGrades.push(grades[i]);
-      } else if ((grades[i] + 1) % 5 === 0) {
-          roundedGrades.push(grades[i] + 1);
-      } else if ((grades[i] + 2) % 5 === 0) {
-          roundedGrades.push(grades[i] + 2);
-      } else {
-          roundedGrades.push(grades[i]);
-      }
+    if (grades[i] < 38) {
+      roundedGrades.push(grades[i])
+    } else if (grades[i] % 5 === 0) {
+      roundedGrades.push(grades[i]);
+    } else if ((grades[i] + 1) % 5 === 0) {
+      roundedGrades.push(grades[i] + 1);
+    } else if ((grades[i] + 2) % 5 === 0) {
+      roundedGrades.push(grades[i] + 2);
+    } else {
+      roundedGrades.push(grades[i]);
+    }
   }
   return roundedGrades;
 }
+
+console.log(gradingStudents([73, 67, 38, 33])) // Answer: [75, 67, 40, 33]
