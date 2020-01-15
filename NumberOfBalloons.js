7f306823f24ea98c704f3f44b9d94b08d963fe0c
aldajani <adajani001@gmail.com>
2020-01-14 23:19:14 -0800

    Implement NumberOfBalloons

---
+++ NumberOfBalloons.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+const maxNumberOfBalloons = (text) => {
+  const textHash = {};
+
+  for (let i = 0; i < text.length; i += 1) {
+    if (!textHash[text[i]]) {
+      textHash[text[i]] = 1;
+    } else {
+      textHash[text[i]] += 1;
+    }
+  }
+
+  let {
+    b, a, l, o, n,
+  } = textHash;
+  let balloons = 0;
+
+  while (b >= 1 && a >= 1 && l >= 2 && o >= 2 && n >= 1) {
+    balloons += 1;
+    b -= 1;
+    a -= 1;
+    l -= 2;
+    o -= 2;
+    n -= 1;
+  }
+
+  return balloons;
+};
+
+console.log(maxNumberOfBalloons('loonbalxballpoon')); // Answer = 2
