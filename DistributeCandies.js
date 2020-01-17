9340566ba4ceb1b5c7eda58745d797ee671455f8
aldajani <adajani001@gmail.com>
2020-01-16 21:26:10 -0800

    Refactor code using new Array to create initialized array with defined spaces

--- DistributeCandies.js
+++ DistributeCandies.js
@@ -1,30 +1,20 @@
 /* eslint-disable no-console */
 const distributeCandies = (candies, numPeople) => {
+  const candySums = new Array(numPeople).fill(0);
   let candiesCopy = candies;
-  const candySums = [];
   let index = 0;
   let counter = 1;
 
   while (candiesCopy > 0) {
     if (index > numPeople - 1) index = 0;
     if (counter > candiesCopy) {
-      if (candySums[index]) {
-        candySums[index] += candiesCopy;
-      } else {
-        candySums[index] = candiesCopy;
-      }
-    } else if (candySums[index]) {
-      candySums[index] += counter;
+      candySums[index] += candiesCopy;
     } else {
-      candySums[index] = counter;
+      candySums[index] += counter;
     }
     index += 1;
     candiesCopy -= counter;
     counter += 1;
-  }
-
-  if (candySums.length !== numPeople) {
-    for (let i = candySums.length; i < numPeople; i += 1) candySums.push(0);
   }
 
   return candySums;
